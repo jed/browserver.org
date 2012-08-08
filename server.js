62e0ee8f91d5a0790566992ab730bfb2f0ee8616
Jed Schmidt <tr@nslator.jp>
2012-08-08 23:22:48 +0900

    comment out browserver for now

--- server.js
+++ server.js
@@ -22,17 +22,17 @@ var client = new Buffer(
 )
 
 browServer.on("connection", function(client) {
-  var hostname = client.id + ".localhost.browserver.org:8001"
+  // var hostname = client.id + ".localhost.browserver.org:8001"
 
-  var req = http.request({
-    method: "PUT",
-    port: 8001,
-    host: client.id + ".localhost.browserver.org",
-    path: "/localhost"
-  })
+  // var req = http.request({
+  //   method: "PUT",
+  //   port: 8001,
+  //   host: client.id + ".localhost.browserver.org",
+  //   path: "/localhost"
+  // })
 
-  req.write(hostname)
-  req.end()
+  // req.write(hostname)
+  // req.end()
 })
 
 function onRequest(req, res) {
