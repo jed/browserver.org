f5976695e0a8843adfc2420b21f360cee9d8d358
Jed Schmidt <tr@nslator.jp>
2012-08-08 23:18:59 +0900

    GitHub for Mac: Throw-away commit.

---
+++ client.js
@@ -0,0 +1,207 @@
+!function() {
+  var ws = new eio.Socket({host: location.hostname, port: location.port})
+
+  var server = http.createServer(function(req, res) {
+    var parts = req.url.split("?")
+
+    req.pathname = parts[0]
+    req.search = parts[1]
+
+    var route = routes[req.pathname]
+
+    if (!route) {
+      res.writeHead(404, {"Content-Type": "text/plain"})
+      return res.end("Not found")
+    }
+
+    var handler = route[req.method]
+
+    if (!handler) {
+      res.writeHead(405, {"Content-Type": "text/plain"})
+      return res.end("Method not allowed")
+    }
+
+    handler.call(this, req, res)
+  })
+
+  server.listen(ws)
+
+  function render(data) {
+    return HTML({lang: "en"},
+      HEAD({},
+        TITLE("෴ browserver: a node.js webserver in your browser ෴"),
+
+        LINK({
+          href: "//fonts.googleapis.com/css?family=Open+Sans:400,800",
+          rel: "stylesheet",
+          type: "text/css"
+        }),
+
+        STYLE({type: "text/css"},
+          CSS("*", {
+            margin: 0,
+            padding: 0
+          }),
+
+          CSS("#container", {
+            width: 800,
+            margin: "0 auto"
+          }),
+
+          CSS("#brow", {
+            marginTop: -250,
+            fontSize: 600,
+            textAlign: "center"
+          }),
+
+          CSS("h1", {
+            marginTop: -200,
+            textAlign: "center",
+            fontFamily: "'Open Sans', sans-serif",
+            fontSize: 100,
+            fontWeight: 800
+          }),
+
+          CSS("h2", {
+            marginTop: -20,
+            textAlign: "center",
+            fontFamily: "'Open Sans', sans-serif",
+            fontSize: 32
+          }),
+
+          CSS("p", {
+            fontFamily: "'Open Sans', sans-serif",
+            fontSize: "1.3em"
+          })
+        )
+      ),
+
+      BODY({},
+        DIV({id: "container"},
+          DIV({id: "brow"}, "෴"),
+
+          H1("browserver"),
+          H2("a node.js HTTP server in your browser"),
+
+          DIV({}, A({href: "http://" + data.host}, "http://" + data.host)),
+
+          PRE(
+            "say `curl " + data.host + "/ask?q=" + encodeURIComponent("What is your name?") + "`"
+          ),
+
+          PRE(
+            "curl " + data.host + "/geolocation"
+          ),
+
+          PRE(
+            "curl -X POST " + data.host + "/rickroll"
+          ),
+
+          P(
+            {id: "blurry"},
+            "For example, to blur this text, enter the following in " +
+            "your terminal:"
+          ),
+
+          PRE(
+            "curl -X PATCH " + data.host + "/style \\",
+            BR({}),
+            "  -d color=transparent \\",
+            BR({}),
+            "  -d text-shadow=\"0 0 5px rgba(0,0,0,0.5)\""
+          )
+        )
+      )
+    )
+  }
+
+  var routes = {
+    "/localhost": {
+      PUT: function(req, res) {
+        document.replaceChild(
+          render({host: req.body}),
+          document.documentElement
+        )
+
+        res.writeHead(204)
+        res.end()
+      }
+    },
+
+    "/ask": {
+      GET: function(req, res) {
+        var match    = req.search.match(/(?:^|&)q=(.+?)(?:$|&)/)
+        var question = match && decodeURIComponent(match[1])
+        var answer   = window.prompt(question)
+
+        res.writeHead(200, {"Content-Type": "text/plain"})
+        res.end(answer + "\n")
+      }
+    },
+
+    "/geolocation": {
+      GET: function(req, res) {
+        if ("geolocation" in navigator) {
+          navigator.geolocation.getCurrentPosition(success, failure)
+        }
+
+        else {
+          res.writeHead(501, {"Content-Type": "text/plain"})
+          return res.end("Not implemented")
+        }
+
+        function success(position) {
+          res.writeHead(200, {"Content-Type": "text/plain"})
+          res.end(
+            "Latitude: " + position.coords.latitude + "\n" +
+            "Longitude: " + position.coords.longitude + "\n"
+          )
+        }
+
+        function failure() {
+          res.writeHead(403, {"Content-Type": "text/plain"})
+          res.end("Forbidden")
+        }
+      }
+    },
+
+    "/style": {
+      PATCH: function(req, res) {
+        var styles = req.body.split("&")
+        var i = styles.length
+        var parts, name, value
+
+        while (i--) {
+          parts = styles[i].split("=")
+
+          name = parts[0].replace(/-[a-z]/g, function(str) {
+            return str.slice(1).toUpperCase()
+          })
+
+          value = decodeURIComponent(parts[1])
+
+          document.getElementById("blurry").style[name] = value
+        }
+
+        res.writeHead(204)
+        res.end()
+      }
+    },
+
+    "/rickroll": {
+      POST: function(req, res) {
+        var win = window.open("http://www.youtube.com/watch?v=oHg5SJYRHA0")
+
+        if (win) {
+          res.writeHead(204)
+          res.end()
+        }
+
+        else {
+          res.writeHead(403, {"Content-Type": "text/plain"})
+          res.end("Forbidden")
+        }
+      }
+    }
+  }
+}()
---
+++ server.js
@@ -0,0 +1,56 @@
+var http = require("http")
+var fs = require("fs")
+
+var brow       = require("brow")
+var browClient = require("brow-client")
+var engine     = require("engine.io")
+
+var httpServer = http.createServer(onRequest)
+var wsServer   = engine.attach(httpServer)
+var browServer = new brow.Server({ws: wsServer, http: httpServer})
+
+var client = new Buffer(
+  "<!doctype html>\n" +
+    "<head>" +
+      "<script>" + fs.readFileSync(__dirname + "/node_modules/engine.io-client/dist/engine.io.js", "utf8") + "</script>\n" +
+      "<script>" + fs.readFileSync(__dirname + "/domo.js", "utf8") + "</script>\n" +
+      "<script>" + browClient.source + "</script>\n" +
+      "<script>" + fs.readFileSync(__dirname + "/client.js", "utf8") + "</script>\n" +
+    "</head>" +
+    "<body>LOADING...</body>" +
+  "</html>"
+)
+
+browServer.on("connection", function(client) {
+  var hostname = client.id + ".localhost.browserver.org:8001"
+
+  var req = http.request({
+    method: "PUT",
+    port: 8001,
+    host: client.id + ".localhost.browserver.org",
+    path: "/localhost"
+  })
+
+  req.write(hostname)
+  req.end()
+})
+
+function onRequest(req, res) {
+  if (req.url == "/") {
+    res.writeHead(200, {
+      "Content-Type": "text/html; charset=utf8",
+      "Content-Length": client.length
+    })
+
+    return res.end(client)
+  }
+
+  res.writeHead(404, {
+    "Content-Type": "text/plain",
+    "Content-Length": 10
+  })
+
+  res.end("Not found\n")
+}
+
+httpServer.listen(process.env.PORT || 8001)
